9086559f4f891998bf5bb9383b088f0518742ef1
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2016-03-21 19:22:23 +0000

    added transaction count + transactions on profile

--- public/profile/ProfileController.js
+++ public/profile/ProfileController.js
@@ -12,17 +12,23 @@ function ProfileController($scope, innergerbil, $q, $stateParams) {
   promises.push(innergerbil.getListResourcePaged($scope.baseUrl + '/partyrelations', {
     from: '/parties/' + $scope.key
   }));
+  promises.push(innergerbil.getListResourcePaged($scope.baseUrl + '/transactions', {
+    involvingParties: '/parties/' + $scope.key,
+    expand: 'results.from,results.to'
+  }));
 
 
   return $q.all(promises).then(function (results) {
     $scope.profile = results[0];
     $scope.contactdetails = results[1].results;
     $scope.partyrelations = results[2].results;
+    $scope.transactions = results[3].results;
     addContactDetailsToParties($scope.profile,$scope.contactdetails);
     splitContactDetails($scope.profile);
     addBalancesOfPartyrelationsToParties($scope.profile, $scope.partyrelations, groupParty)
-
-    console.log('$scope.profile ->');
-    console.log($scope.profile); // eslint-disable-line
+//    console.log('$scope.profile ->');
+//    console.log($scope.profile); // eslint-disable-line
+console.info('$scope.transactions');
+console.info($scope.transactions);
   });
 }
