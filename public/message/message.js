9e6d31d6728578ed3197246c6a7da602d28733c3
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2016-04-13 14:35:26 +0000

    isolated scope for message directive

--- public/message/message.js
+++ public/message/message.js
@@ -1,6 +1,10 @@
 function messageDirective() {
   return {
     restrict: 'E',
+    scope: {
+      event: '=message',
+      baseUrl: '='
+    },
     templateUrl: function(elem, attr){
       return 'message/message.html';
     }
